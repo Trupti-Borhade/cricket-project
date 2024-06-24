61da71dad840ecdf67b9312252adf3a451f2cdc5
Trupti-Borhade <167518208+Trupti-Borhade@users.noreply.github.com>
2024-06-24 17:34:14 +0530

    Update TestRunner.java

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "features",
         glue = "stepdef",
-        tags = "@smoke",
+        tags = "@add-player",
         plugin = { "pretty",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 }
